1a57134f6578230e80f58dd657d33cd9541d4d86
Samuel Ribeiro <samuellr22@gmail.com>
2026-08-25 21:50:01 -0300

    add exercicios_py

---
+++ POO/Exercicios_py/exercicio1.py
@@ -0,0 +1,9 @@
+class Musica:
+    def __init__(self, nome ='', artista='', duração=0)
+    self.nome = nome
+    self.artista = artista
+    self.duração = duração
+
+    musica1 = Musica(nome='Crick', artista='Mercury', duração=200)
+    musica2 = Musica(nome='The Dance', artista='Random', duração=400)
+    musica3 = Musica(nome='Moth', artista='Lilili', duração=500)
