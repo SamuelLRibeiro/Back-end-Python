2f84cf67017a674d73f49037370c160a7848a3ad
Samuel Ribeiro <samuellr22@gmail.com>
2026-08-28 20:38:16 -0300

    new folder

---
+++ POO/poo_course/app.py
@@ -0,0 +1,14 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Lais', 8)
+#restaurante_mexicano = Restaurante('mexican food', 'Comida Mexicana')
+#restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+#restaurante_mexicano.alternar_estado()
+
+def main():
+    Restaurante.listar_restaurantes()
+if __name__ == '__main__':
+    main()
